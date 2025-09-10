c1e72474ead9049dfb73eba728e2fce57437bce5
Didora Qazoqova <didoraqazoqova421@gmail.com>
2025-09-10 17:49:45 +0500

    my homework

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+let userName = "Didora";
+let age = 16;
+let city = "Khorugh";
+
+console.log(`My name is ${userName}, I am ${age} year, I am from ${city} city`);
+
+if (age >=18){
+    console.log("Sovershennoletniy");
+} else{
+    console.log("Nesovershennoletniy");
+}
+
+function greet(name){
+    console.log(`Welcome, ${name}`);
+    
+}
+
+greet("Didora");
+
+function miltiply(a, b){
+    return a*b;
+}
+console.log(miltiply(10, 20));
+
+for(let i=1; i<=10; i++){
+    if(i%2===0){
+        console.log(`${i}-chotnoe`);
+    }else{
+        console.log(`${i}-nechotnoe`);
+    }
+}
+
 
